085bee4ddfc81743c1f701f6d548b2910fa75c45
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 14:01:42 +0530

    MultiplicationofTwoNumber using datatype with short

---
+++ arithmetic-operators/src/operatorexamples/multiplication/usingdatatype/MultiplicationofTwoNumber_withshort.java
@@ -0,0 +1,14 @@
+package operatorexamples.multiplication.usingdatatype;
+
+public class MultiplicationofTwoNumber_withshort {
+
+    public static void main(String[] args) {
+
+        short val1 = 16;
+        short val2 = 5;
+
+        int result = val1 * val2;
+
+        System.out.println("Multiplication of " + val1 + " * " + val2 + " = " + result);
+    }
+}
