1011d923e8a7562e8ef74156758e4feb950eb0e6
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 12:30:41 +0530

    AdditionofTwoNumber using datatype with double..

---
+++ arithmetic-operators/src/operatorexamples/addition/usingdatatype/AdditionofTwoNumbers_withdouble.java
@@ -0,0 +1,14 @@
+package operatorexamples.addition.usingdatatype;
+
+public class AdditionofTwoNumbers_withdouble {
+
+    public static void main(String[] args) {
+
+        double num1 = 132.55d;
+        double num2 = 254.44d;
+
+        double sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " + " + num2 + " = " + sum);
+    }
+}
