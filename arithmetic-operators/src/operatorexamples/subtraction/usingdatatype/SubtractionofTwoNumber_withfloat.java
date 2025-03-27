5bbe13a8f4424a22f89cb55ee1a0f437bd37f8f7
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:41:59 +0530

    SubtractionofTwoNumber using datatype with float

---
+++ arithmetic-operators/src/operatorexamples/subtraction/usingdatatype/SubtractionofTwoNumber_withfloat.java
@@ -0,0 +1,14 @@
+package operatorexamples.subtraction.usingdatatype;
+
+public class SubtractionofTwoNumber_withfloat {
+
+    public static void main(String[] args) {
+
+        float val1 = 15.5f;
+        float val2 = 8.3f;
+
+        float result = val1 - val2;
+
+        System.out.println("Subtraction of " + val1 + " - " + val2 + " = " + result);
+    }
+}
