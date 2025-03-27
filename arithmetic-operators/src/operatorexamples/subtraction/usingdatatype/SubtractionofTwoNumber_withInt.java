62fae2c2075ec27b12c89bcd71acec4220d5ed07
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:26:33 +0530

    Added SubtractionofTwoNumber using datatype with int

---
+++ arithmetic-operators/src/operatorexamples/subtraction/usingdatatype/SubtractionofTwoNumber_withInt.java
@@ -0,0 +1,14 @@
+package operatorexamples.subtraction.usingdatatype;
+
+public class SubtractionofTwoNumber_withInt {
+
+    public static void main(String[] args) {
+
+        int val1 = 56;
+        int val2 = 25;
+
+        int result = val1 - val2;
+
+        System.out.println("Subtraction of " + val1 + " - " + val2 + " = " + result);
+    }
+}
