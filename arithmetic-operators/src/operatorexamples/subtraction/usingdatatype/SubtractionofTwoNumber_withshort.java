3aaa5937da3f7c74669fd1b5f74f6ea40ea3936f
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:45:37 +0530

    SubtractionofTwoNumber using datatype with short

---
+++ arithmetic-operators/src/operatorexamples/subtraction/usingdatatype/SubtractionofTwoNumber_withshort.java
@@ -0,0 +1,14 @@
+package operatorexamples.subtraction.usingdatatype;
+
+public class SubtractionofTwoNumber_withshort {
+
+    public static void main(String[] args) {
+
+        short val1 = 15;
+        short val2 = 6;
+
+        int result = val1 - val2;
+
+        System.out.println("Subtraction of " + val1 + " - " + val2 + " = " + result);
+    }
+}
