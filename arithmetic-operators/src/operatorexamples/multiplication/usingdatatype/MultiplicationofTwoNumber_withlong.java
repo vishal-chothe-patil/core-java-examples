74d431c8dbc0a34c365eb7576efdbef4ecf0c5dd
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:56:24 +0530

    MultiplicationofTwoNumber using datatype with long

---
+++ arithmetic-operators/src/operatorexamples/multiplication/usingdatatype/MultiplicationofTwoNumber_withlong.java
@@ -0,0 +1,14 @@
+package operatorexamples.multiplication.usingdatatype;
+
+public class MultiplicationofTwoNumber_withlong {
+
+    public static void main(String[] args) {
+
+        long val1 = 155L;
+        long val2 = 100L;
+
+        long result = val1 * val2;
+
+        System.out.println("Multiplication of " + val1 + " * " + val2 + " = " + result);
+    }
+}
