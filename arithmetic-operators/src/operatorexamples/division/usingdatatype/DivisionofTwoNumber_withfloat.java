f436c82685f9cea37bd79c504c491a05d74e5328
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 14:09:44 +0530

    DivisionofTwoNumber using datatype with float

---
+++ arithmetic-operators/src/operatorexamples/division/usingdatatype/DivisionofTwoNumber_withfloat.java
@@ -0,0 +1,14 @@
+package operatorexamples.division.usingdatatype;
+
+public class DivisionofTwoNumber_withfloat {
+
+    public static void main(String[] args) {
+
+        float val1 = 565.5f;
+        float val2 = 5f;
+
+        float result = val1 / val2;
+
+        System.out.println("Division of " + val1 + " / " + val2 + " = " + result);
+    }
+}
