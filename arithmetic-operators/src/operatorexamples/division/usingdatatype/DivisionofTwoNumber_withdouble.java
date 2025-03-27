43f2596f5328abe80bb6b3543b4976cd07fd8e26
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 14:12:09 +0530

    DivisionofTwoNumber using datatype with double

---
+++ arithmetic-operators/src/operatorexamples/division/usingdatatype/DivisionofTwoNumber_withdouble.java
@@ -0,0 +1,14 @@
+package operatorexamples.division.usingdatatype;
+
+public class DivisionofTwoNumber_withdouble {
+
+    public static void main(String[] args) {
+
+        double val1 = 555.5d;
+        double val2 = 5.5d;
+
+        double result = val1 / val2;
+
+        System.out.println("Division of " + val1 + " / " + val2 + " = " + result);
+    }
+}
