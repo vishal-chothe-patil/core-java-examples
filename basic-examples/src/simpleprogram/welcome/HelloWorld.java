9275082926fbe04b427435b2fd22e78047ef42ef
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:08:56 +0530

    added comments it help to understand each part of program..

--- basic-examples/src/simpleprogram/welcome/HelloWorld.java
+++ basic-examples/src/simpleprogram/welcome/HelloWorld.java
@@ -1,7 +1,14 @@
+// Package declaration - it specifies the package name of this class.
 package simpleprogram.welcome;
 
+// Class declaration - defines a public class named HelloWorld.
 public class HelloWorld {
+
+    // Main method - this is the entry point of any Java application.
+    // The Java Virtual Machine (JVM) looks for this method to start the program.
     public static void main(String[] args) {
+
+        // prints the text "Hello, world! and welcome!" to the console.
         System.out.println("Hello, world! and welcome!");
     }
 }
