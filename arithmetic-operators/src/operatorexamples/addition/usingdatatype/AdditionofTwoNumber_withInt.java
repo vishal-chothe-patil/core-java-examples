4551a09e32d0a991fd1a10016dc4dac7d5a03a3c
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 12:16:33 +0530

    AdditionofTwoNumber using Datatype with int..

---
+++ arithmetic-operators/src/operatorexamples/addition/usingdatatype/AdditionofTwoNumber_withInt.java
@@ -0,0 +1,24 @@
+package operatorexamples.addition.usingdatatype;
+
+public class AdditionofTwoNumber_withInt {
+
+    // Question: WAP to add two numbers using int data type and print the result?
+    // Pseudocode: (steps)
+    // Step 1: Declare and initialize two integer variables with values
+    // Step 2: Add the two integers and
+    // Step 3: Store the result in a third variable
+    // Step 4: Print the result
+
+    public static void main(String[] args) {
+
+        // Step 1: Declare and initialize two integer numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Step 2: Add and Calculate the sum of the two numbers
+        int sum = num1 + num2; // Step 3: Store in third variable 'sum'
+
+        // Step 3: Print the result
+        System.out.println("Sum of " + num1 + " + " + num2 + " = " + sum);
+    }
+}
