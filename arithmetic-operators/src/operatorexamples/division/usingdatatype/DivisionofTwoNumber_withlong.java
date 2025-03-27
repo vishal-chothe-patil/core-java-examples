b9ce80f51f8b4b3e3331f2e957d24407cb210466
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 14:06:18 +0530

    DivisionofTwoNumber using datatype with long

---
+++ arithmetic-operators/src/operatorexamples/division/usingdatatype/DivisionofTwoNumber_withlong.java
@@ -0,0 +1,14 @@
+package operatorexamples.division.usingdatatype;
+
+public class DivisionofTwoNumber_withlong {
+
+    public static void main(String[] args) {
+
+        long val1 = 5656L;
+        long val2 = 5L;
+
+        long result = val1 / val2;
+
+        System.out.println("Division of " + val1 + " / " + val2 + " = " + result);
+    }
+}
