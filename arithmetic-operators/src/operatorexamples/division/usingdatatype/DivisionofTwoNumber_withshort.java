f77fe102eb7df7cf8dd6a998b3cbb3e40adab509
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 14:13:45 +0530

    DivisionofTwoNumber using datatype with short

---
+++ arithmetic-operators/src/operatorexamples/division/usingdatatype/DivisionofTwoNumber_withshort.java
@@ -0,0 +1,14 @@
+package operatorexamples.division.usingdatatype;
+
+public class DivisionofTwoNumber_withshort {
+
+    public static void main(String[] args) {
+
+        short val1 = 111;
+        short val2 = 10;
+
+        int result = val1 / val2;
+
+        System.out.println("Division of " + val1 + " / " + val2 + " = " + result);
+    }
+}
