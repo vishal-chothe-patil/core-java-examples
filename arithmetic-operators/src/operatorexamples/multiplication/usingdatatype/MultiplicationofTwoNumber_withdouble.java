400707c007df188a8ccd27da5851735251c4b12b
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:59:48 +0530

    MultiplicationofTwoNumber using datatype with double

---
+++ arithmetic-operators/src/operatorexamples/multiplication/usingdatatype/MultiplicationofTwoNumber_withdouble.java
@@ -0,0 +1,14 @@
+package operatorexamples.multiplication.usingdatatype;
+
+public class MultiplicationofTwoNumber_withdouble {
+
+    public static void main(String[] args) {
+
+        double val1 = 133.5d;
+        double val2 = 10.5d;
+
+        double result = val1 * val2;
+
+        System.out.println("Multiplication of " + val1 + " * " + val2 + " = " + result);
+    }
+}
