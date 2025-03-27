cdb93d41fd3815191339963683dcd6ffca944229
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:43:43 +0530

    SubtractionofTwoNumber using datatype with double

---
+++ arithmetic-operators/src/operatorexamples/subtraction/usingdatatype/SubtractionofTwoNumber_withdouble.java
@@ -0,0 +1,14 @@
+package operatorexamples.subtraction.usingdatatype;
+
+public class SubtractionofTwoNumber_withdouble {
+
+    public static void main(String[] args) {
+
+        double val1 = 153.5;
+        double val2 = 84.3;
+
+        double result = val1 - val2;
+
+        System.out.println("Subtraction of " + val1 + " - " + val2 + " = " + result);
+    }
+}
