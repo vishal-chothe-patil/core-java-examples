51bdc009d2a498cb2049756d13d5607731d5287c
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 12:23:57 +0530

    AdditionofTwoNumber using datatype with float added..

---
+++ arithmetic-operators/src/operatorexamples/addition/usingdatatype/AdditionofTwoNumber_withfloat.java
@@ -0,0 +1,14 @@
+package operatorexamples.addition.usingdatatype;
+
+public class AdditionofTwoNumber_withfloat {
+
+    public static void main(String[] args) {
+
+        float num1 = 13.5f;
+        float num2 = 25.4f;
+
+        float sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " + " + num2 + " = " + sum);
+    }
+}
