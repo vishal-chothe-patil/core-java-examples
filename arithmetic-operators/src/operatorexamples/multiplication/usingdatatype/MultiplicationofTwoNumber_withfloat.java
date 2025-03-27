2eb01cf79b11c50e29df5e3101812470e0206214
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:58:09 +0530

    MultiplicationofTwoNumber using datatype with float

---
+++ arithmetic-operators/src/operatorexamples/multiplication/usingdatatype/MultiplicationofTwoNumber_withfloat.java
@@ -0,0 +1,14 @@
+package operatorexamples.multiplication.usingdatatype;
+
+public class MultiplicationofTwoNumber_withfloat {
+
+    public static void main(String[] args) {
+
+        float val1 = 13.5f;
+        float val2 = 10.6f;
+
+        float result = val1 * val2;
+
+        System.out.println("Multiplication of " + val1 + " * " + val2 + " = " + result);
+    }
+}
