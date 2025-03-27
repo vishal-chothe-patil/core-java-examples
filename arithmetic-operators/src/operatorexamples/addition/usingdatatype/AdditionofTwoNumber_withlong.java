1cc45d753e3ec67da1725dd9d36c7ad0fbaefa18
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 12:20:20 +0530

    AdditionofTwoNumber using datatype with long..

---
+++ arithmetic-operators/src/operatorexamples/addition/usingdatatype/AdditionofTwoNumber_withlong.java
@@ -0,0 +1,14 @@
+package operatorexamples.addition.usingdatatype;
+
+public class AdditionofTwoNumber_withlong {
+
+    public static void main(String[] args) {
+
+        long num1 = 103L;
+        long num2 = 205L;
+
+        long sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " + " + num2 + " = " + sum);
+    }
+}
