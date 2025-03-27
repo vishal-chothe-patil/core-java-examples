c50fe2f89ae378cb6b589d3ae85b9fbfdb11c24c
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 16:54:20 +0530

    MultiplicationofTwoNumber using methods without return type

--- arithmetic-operators/src/operatorexamples/usingmethods/withoutreturntype/MultiplicationOfTwoNumbersWithoutReturnType.java
+++ arithmetic-operators/src/operatorexamples/usingmethods/withoutreturntype/MultiplicationOfTwoNumbersWithoutReturnType.java
@@ -2,38 +2,38 @@ package operatorexamples.usingmethods.withoutreturntype;
 
 public class MultiplicationOfTwoNumbersWithoutReturnType {
 
-    public void mulInt() {
+    public void multiInt() {
         int val1 = 8, val2 = 9;
         int result = val1 * val2;
         System.out.println("Multiplication of Two Integers: " + val1 + " * " + val2 + " = " + result);
     }
 
-    public void mulLong() {
+    public void multiLong() {
         long a = 50L, b = 60L;
         System.out.println("Multiplication of Two Longs: " + a + " * " + b + " = " + (a * b));
     }
 
-    public void mulFloat() {
+    public void multiFloat() {
         float x = 7.5f, y = 2.0f;
         System.out.println("Multiplication of Two Floats: " + x + " * " + y + " = " + (x * y));
     }
 
-    public void mulDouble() {
+    public void multiDouble() {
         double p = 12.5d, q = 4.2d;
         System.out.println("Multiplication of Two Doubles: " + p + " * " + q + " = " + (p * q));
     }
 
-    public void mulShort() {
+    public void multiShort() {
         short m = 4, n = 5;
         System.out.println("Multiplication of Two Shorts: " + m + " * " + n + " = " + (m * n));
     }
 
     public static void main(String[] args) {
-        MultiplicationOfTwoNumbersWithoutReturnType mulObj = new MultiplicationOfTwoNumbersWithoutReturnType();
-        mulObj.mulInt();
-        mulObj.mulLong();
-        mulObj.mulFloat();
-        mulObj.mulDouble();
-        mulObj.mulShort();
+        MultiplicationOfTwoNumbersWithoutReturnType multiObj = new MultiplicationOfTwoNumbersWithoutReturnType();
+        multiObj.multiInt();
+        multiObj.multiLong();
+        multiObj.multiFloat();
+        multiObj.multiDouble();
+        multiObj.multiShort();
     }
 }
