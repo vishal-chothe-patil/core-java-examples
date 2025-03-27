607cd01532f9ebe8985ca2583a5e2856fe8b2a73
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:52:27 +0530

    MultiplicationofTwoNumber using datatype with int

---
+++ arithmetic-operators/src/operatorexamples/multiplication/usingdatatype/MultiplicationofTwoNumber_withInt.java
@@ -0,0 +1,14 @@
+package operatorexamples.multiplication.usingdatatype;
+
+public class MultiplicationofTwoNumber_withInt {
+
+    public static void main(String[] args) {
+
+        int val1 = 15;
+        int val2 = 10;
+
+        int result = val1 * val2;
+
+        System.out.println("Multiplication of " + val1 + " * " + val2 + " = " + result);
+    }
+}
