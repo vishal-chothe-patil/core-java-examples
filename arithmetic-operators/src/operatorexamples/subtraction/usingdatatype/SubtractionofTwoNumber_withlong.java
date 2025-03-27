cb3d377dd1e43e0bd787ad0c65ef967bea441e7e
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 13:39:31 +0530

    added SubtractionofTwoNumber using datatype with long

---
+++ arithmetic-operators/src/operatorexamples/subtraction/usingdatatype/SubtractionofTwoNumber_withlong.java
@@ -0,0 +1,14 @@
+package operatorexamples.subtraction.usingdatatype;
+
+public class SubtractionofTwoNumber_withlong {
+
+    public static void main(String[] args) {
+
+        long val1 = 5656L;
+        long val2 = 2525L;
+
+        long result = val1 - val2;
+
+        System.out.println("Subtraction of " + val1 + " - " + val2 + " = " + result);
+    }
+}
