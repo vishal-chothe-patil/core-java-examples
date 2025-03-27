d6d9ff42a0f790bca4ee666843a8dc79d25fe7fc
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-27 14:04:29 +0530

    DivisionofTwoNumber using datatype with int

---
+++ arithmetic-operators/src/operatorexamples/division/usingdatatype/DivisionofTwoNumber_withInt.java
@@ -0,0 +1,14 @@
+package operatorexamples.division.usingdatatype;
+
+public class DivisionofTwoNumber_withInt {
+
+    public static void main(String[] args) {
+
+        int val1 = 56;
+        int val2 = 5;
+
+        int result = val1 / val2;
+
+        System.out.println("Subtraction of " + val1 + " / " + val2 + " = " + result);
+    }
+}
