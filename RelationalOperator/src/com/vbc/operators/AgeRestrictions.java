eb49350cab56dcdaf94c67f6e49a3e77830354e5
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-29 12:14:08 +0530

    AgeRestrictions example added

---
+++ RelationalOperator/src/com/vbc/operators/AgeRestrictions.java
@@ -0,0 +1,20 @@
+package com.vbc.operators;
+
+import java.util.Scanner;
+
+public class AgeRestrictions {
+    // Age Restriction : Check if a user is too young to access specific content.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Age : ");
+        int age = scanner.nextInt();
+
+        if (age < 18) {
+            System.out.println("Access Denied: You must be at least 18 years old.");
+        } else {
+            System.out.println("Access Granted. you explore now..");
+        }
+
+    }
+}
