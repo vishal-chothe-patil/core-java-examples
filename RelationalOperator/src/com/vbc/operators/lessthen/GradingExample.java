73595d706f5c77d544d4510b7a049e902dc794c4
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-03-29 12:33:18 +0530

    Grading score example added

---
+++ RelationalOperator/src/com/vbc/operators/lessthen/GradingExample.java
@@ -0,0 +1,20 @@
+package com.vbc.operators.lessthen;
+
+import java.util.Scanner;
+
+public class GradingExample {
+    // Grading : Determine if a score is below a passing grade.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Score :");
+        double score = Double.parseDouble(scanner.nextLine());
+
+        if (score < 35) {
+            System.out.println("Your Result is Fail. Minimum 35 marks required to pass.");
+        } else {
+            System.out.println("Your Result is Pass");
+        }
+
+    }
+}
