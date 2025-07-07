8db324cee8397a1d58a00503cadb0de8e605dd91
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-07-07 14:43:50 +0530

    init: added java-design-pattern project example

---
+++ java-design-pattern/src/DesignPatternExamples.java
@@ -0,0 +1,17 @@
+public class DesignPatternExamples {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome to Java Design Pattern!");
+
+        String[] patterns = {
+                "1. Simple (Direct)",
+                "2. Factory Pattern",
+                "3. Singleton Pattern",
+                "4. Builder Pattern",
+                "5. Prototype Pattern"
+        };
+
+        for (String pattern : patterns) {
+            System.out.println(pattern);
+        }
+    }
+}
